ff8ee42e7c0d06b8294f36442f2a11bcefa58cfb
Kevin Brayan <kevinbrayanteixeira@gmail.com>
2026-07-12 14:54:52 -0300

    commit inicial

---
+++ src/main/java/dev/kevin/desafio_2026_2_java/Desafio20262JavaApplication.java
@@ -0,0 +1,13 @@
+package dev.kevin.desafio_2026_2_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio20262JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio20262JavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kevin/desafio_2026_2_java/Desafio20262JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kevin.desafio_2026_2_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio20262JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
