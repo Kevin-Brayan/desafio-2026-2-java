27a3aff1cf3c595b8303a8dde3728caf292e5841
Kevin Brayan <kevinbrayanteixeira@gmail.com>
2026-07-15 23:50:49 -0300

    cria endpoint para consultar solicitacoes (/solicitacao/listar)

--- src/main/java/dev/kevin/desafio_2026_2_java/controller/SolicitacaoController.java
+++ src/main/java/dev/kevin/desafio_2026_2_java/controller/SolicitacaoController.java
@@ -4,10 +4,10 @@ import dev.kevin.desafio_2026_2_java.dto.SolicitacaoDTO;
 import dev.kevin.desafio_2026_2_java.entity.Solicitacao;
 import dev.kevin.desafio_2026_2_java.service.SolicitacaoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/solicitacao")
@@ -21,5 +21,19 @@ public class SolicitacaoController {
 
         return solicitacaoService.cadastrarSolicitacao(solicitacaoRequest);
 
+    }
+
+    @GetMapping("/listar")
+    public Page<SolicitacaoDTO> listarSolicitacao(
+                @RequestParam(required = false) String aluno,
+                @RequestParam(required = false) String curso,
+                @RequestParam(required = false) String status,
+                @RequestParam(required = false) String tipoDocumento,
+                @RequestParam(required = false) LocalDate inicio,
+                @RequestParam(required = false) LocalDate fim,
+                @RequestParam(defaultValue = "0") int page,
+                @RequestParam(defaultValue = "10") int size
+            ){
+        return solicitacaoService.listarSolicitacao(page, size);
     }
 }
--- src/main/java/dev/kevin/desafio_2026_2_java/service/SolicitacaoService.java
+++ src/main/java/dev/kevin/desafio_2026_2_java/service/SolicitacaoService.java
@@ -6,6 +6,9 @@ import dev.kevin.desafio_2026_2_java.entity.Solicitacao;
 import dev.kevin.desafio_2026_2_java.mapper.SolicitacaoMapper;
 import dev.kevin.desafio_2026_2_java.repository.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -49,6 +52,15 @@ public class SolicitacaoService {
         solicitacaoRepository.save(solicitacao);
 
         return solicitacaoMapper.map(solicitacao);
+
+    }
+
+    public Page<SolicitacaoDTO> listarSolicitacao(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+
+        Page<Solicitacao> pagina = solicitacaoRepository.findAll(pageable);
+
+        return pagina.map(solicitacaoMapper::map);
 
     }
 
